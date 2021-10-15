40e056fc4f12ddf3232532a91bcc2a6eeec602e2
jwc12551 <jacobchristensen12@gmail.com>
2021-10-15 12:21:39 -0600

    bin

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -65,7 +65,22 @@ class Calculator {
     if int a = 16 then this method returns: 10000
      */
     String intToBinaryNumber(int n){
-        return null;
+        // Creating and assigning binary array size
+        int[] binary = new int[35];
+        int id = 0;
+ 
+        // Number should be positive
+        while (num > 0) {
+            binary[id++] = num % 2;
+            num = num / 2;
+        }
+ 
+        StringBuilder sb = new StringBuilder()
+        for (int i = id - 1; i >= 0; i--) {
+            sb.append(binary[i] + "");
+        }
+
+        return sb.toString();
     }
 
     /*
