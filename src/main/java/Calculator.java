78d5bef3f564b185aa5a8450f431dcb912eb7349
jwc12551 <jacobchristensen12@gmail.com>
2021-10-21 21:48:00 -0600

    Changed fibonacci to dynamic progamming

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -41,10 +41,22 @@ class Calculator {
     etc
      */
     int fibonacciNumberFinder(int n){
-        if ((n == 0) || (n == 1))
-            return n;
-        else
-            return fibonacciNumberFinder(n - 1) + fibonacciNumberFinder(n - 2);
+      /* Declare an array to store Fibonacci numbers. */
+      int f[] = new int[n+2]; // 1 extra to handle case, n = 0
+      int i;
+
+      /* 0th and 1st number of the series are 0 and 1*/
+      f[0] = 0;
+      f[1] = 1;
+
+      for (i = 2; i <= n; i++)
+      {
+         /* Add the previous 2 numbers in the series
+           and store it */
+          f[i] = f[i-1] + f[i-2];
+      }
+
+      return f[n];
     }
 
 
