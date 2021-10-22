9ddec95a4a3c57983831b0e03fed0b085c19cea5
jwc12551 <jacobchristensen12@gmail.com>
2021-10-21 23:20:54 -0600

    Added Main.java

---
+++ src/main/java/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+
+  public static void main(String[] args) {
+    Calculator calculator = new Calculator();
+
+    switch (args[0]) {
+      case "add":
+        System.out.println(calculator.add(Integer.parseInt(args[1]), Integer.parseInt(args[2])));
+        break;
+      case "subtract":
+        System.out.println(calculator.subtract(Integer.parseInt(args[1]), Integer.parseInt(args[2])));
+        break;
+      case "multiply":
+        System.out.println(calculator.multiply(Integer.parseInt(args[1]), Integer.parseInt(args[2])));
+        break;
+      case "divide":
+        System.out.println(calculator.divide(Integer.parseInt(args[1]), Integer.parseInt(args[2])));
+        break;
+      case "fibonacci":
+        System.out.println(calculator.fibonacciNumberFinder(Integer.parseInt(args[1])));
+        break;
+      case "binary":
+        System.out.println(calculator.intToBinaryNumber(Integer.parseInt(args[1])));
+        break;
+    }
+  }
+
+}
